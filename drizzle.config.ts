57c24b8982ade55ac032be54aebcbe90e0e9b5a1
Caio Gallo <caiodgallo@Caios-MacBook-Air.local>
2025-12-29 17:05:09 -0300

    feat: testing deploy

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
   dialect: 'sqlite',
   dbCredentials: {
     url: process.env.TURSO_DATABASE_URL || process.env.DATABASE_URL || 'file:./local.db',
-    authToken: process.env.TURSO_AUTH_TOKEN,
+    // authToken: process.env.TURSO_AUTH_TOKEN,
   },
 });
